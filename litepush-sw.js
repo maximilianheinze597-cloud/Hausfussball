7ffdf423508d3ed9e20260848cb5dfcb2cce9e54
Jedi-Archive <maximilianheinze597@gmail.com>
2026-08-27 12:13:57 +0200

    Delete litepush-sw.js

--- litepush-sw.js
+++
@@ -1,6 +0,0 @@
-<script
-  src="https://litepush.dev/sdk.js"
-  data-project="prj_01M10ZZMB8FQ0P9199F3YTSQTK"
-  data-vapid-key="BGdRmdaHmTwhqfNgmG_EHRGl_o_giq1Gg79eE-vVV5D0fuKsN4ilelPMibfezCxLcfvXTP-Vede3iJfXlgX6hvQ"
-  async
-></script>
